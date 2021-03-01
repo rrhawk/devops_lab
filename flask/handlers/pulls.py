b96b9fbbdbaaa6246c4fdf6cf24089bf2df22afb
Sergey Kosolapov <Sergey_Kosolapov@epam.com>
2021-03-01 17:44:21 +0300

    Homework5: Sergey Kosolapov

---
+++ flask/handlers/pulls.py
@@ -0,0 +1,31 @@
+import requests
+
+headers = {'Authorization': 'token %s' % "8f038539eb3b568b8404df754aa1e300fff1136d", 'per_page': 100, 'state': 'all'}
+r = requests.get('https://api.github.com/repos/alenaPy/devops_lab/pulls', params=headers)
+
+
+def get_pulls(state):
+    ls = []
+
+    def add_pulls(j):
+        pulls = {
+            "num": j["number"],
+            "title": j["title"],
+            "link": j["html_url"]
+        }
+        return ls.append(pulls)
+    for i in r.json():
+        if i["labels"]:
+            if state == "open":
+                if i["state"] == 'open':
+                    add_pulls(i)
+            elif state == "closed":
+                if i["state"] == "closed":
+                    add_pulls(i)
+            elif state == "accepted":
+                if i["labels"][0]["name"] == "accepted":
+                    add_pulls(i)
+            elif state == "needs work":
+                if i["labels"][0]["name"] == "needs work":
+                    add_pulls(i)
+    return ls
